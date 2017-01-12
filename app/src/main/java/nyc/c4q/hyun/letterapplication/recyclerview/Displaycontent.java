8f60a5529e8e2a53940dfd26c0ceb2381f103c6b
hyunjkim <hkim6501@gmail.com>
2017-01-11 21:59:06 -0500

    second commit

--- app/src/main/java/nyc/c4q/hyun/letterapplication/recyclerview/Displaycontent.java
+++ app/src/main/java/nyc/c4q/hyun/letterapplication/recyclerview/Displaycontent.java
@@ -1,9 +1,11 @@
 package nyc.c4q.hyun.letterapplication.recyclerview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -27,9 +29,24 @@ public class Displaycontent extends AppCompatActivity{
 
         Bundle fromLetterVH = getIntent().getExtras();
         if(fromLetterVH!=null){
-            url = fromLetterVH.getString("thephoto");
+            url = fromLetterVH.getString("thephoto",url);
             Picasso.with(this).load(API_URL+url).fit().into(imageView);
         }
+
+    }
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this,"Heading back soon", Toast.LENGTH_LONG).show();
+        int i = 0;
+        if(i<1){
+            onBackPressed();
+            i++;
+        } else {
+            Intent intent = new Intent(this, Displaycontent.class);
+            intent.putExtra("toActivity",1);
+            startActivity(intent);
+        }
+        super.onBackPressed();
 
     }
 }
